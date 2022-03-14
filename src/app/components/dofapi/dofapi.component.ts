7a94d6b991eb8d81a81b32efb28363d6e8a0a8ff
Killourmeu <abel.coutolleau@gmail.com>
2022-03-14 15:29:53 +0100

    Dofapi Front Champs Runes
    Saisie du nombre de runes au début & à la fin + nom des champs + stockage opti dans des classes stylées

--- src/app/components/dofapi/dofapi.component.ts
+++ src/app/components/dofapi/dofapi.component.ts
@@ -1,22 +1,154 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+//import * as internal from 'stream';
 import { ApiService } from './../../services/api.service';
+import { Carac } from './../../services/api.service';
+import { Rune } from './../../services/api.service';
 
 @Component({
   selector: 'app-dofapi',
   templateUrl: './dofapi.component.html',
   styleUrls: ['./dofapi.component.css']
 })
+
 export class DofapiComponent implements OnInit {
+//##### Variables #####
+  // Collection contenant les différentes caractéristiques & types de runes
+  tCaracs: Carac[]
+  nomItemRecherche: string = "";
+  //exo: Carac = new Carac("","",0);
+  tabItemsJSON: any = [];
+  item: any = [];
+  statsItem: Carac[] = new Array<Carac>();
 
-  Dofusteuse:any = [];
+  /* Variables à créer :
+        - []Ensemble des Items (+stockage)
+        - Nom de l'item
+        - []Caractéristiques de l'item (?)
+        - Exo
+        - PrixTotal
+  */
 
-  constructor( private apiService: ApiService) { }
+// Constructeur
+  constructor( private apiService: ApiService) {
+    this.tCaracs = apiService.getStatistiques();
+    console.log(this.item);
+  }
 
+// Méthodes
+
+/*  Au lancement de la page :
+ *    - Récupération de l'entiereté des items & stockage en JSON
+ */ 
   ngOnInit(): void {
-    this.apiService.GetDofusteuse().subscribe(res => {
-      //console.log(res)
-      this.Dofusteuse = res;
+    this.apiService.GetBottesHarry().subscribe(res => {
+      console.log(res);
+      this.item = JSON.parse(JSON.stringify(res));
+      this.lancerPagePlease();
+      console.log("TEST", this.item);
     });
+       
+  }
+
+  lancerPagePlease(): void{
+    this.getStatsItem(); 
+    console.log("Stats de l'objet : ", this.statsItem);
+  }
+
+/*  Lorsque l'utilisateur recherche un item : 
+ *    TRIGGER : barre rechercher.keyup
+ *    - Récupère dans la base de données JSON stockée l'item en question
+ *    - (+ Prédiction de frappe dans la barre de recherche)
+ *    - Lance l'initialisation des cartes de FMs selon les statistiques
+ *    - Lance l'initialisation du menu des prix selon les statistiques
+ */
+  rechercherItem(): void {
+    
+  }
+
+/*  Initialisation d'une ligne de FM :
+ *    TRIGGER : recherche correcte
+ *    - Prend une stat en paramètre
+ *    - Initialise les dépendances aux champs de texte
+ */
+  getStatsItem(): void {
+    let i, j;
+    console.log(this.tCaracs.length);
+    console.log(this.tCaracs);
+    console.log("item : ", this.item[0].statistics[0]);
+    console.log("stat", this.item[0].statistics.length, this.item[0].statistics[4]);
+    console.log("testKeys :", Object.keys(this.item[0].statistics[7])[0]);
+    
+    for( i = 0 ; i < this.tCaracs.length ; i++ ){
+      for( j = 0 ; j < this.item[0].statistics.length ; j++ ){
+        if(Object.keys(this.item[0].statistics[j])[0] == this.tCaracs[i].nom){
+          console.log(Object.keys(this.item[0].statistics[j])[0]);
+          this.statsItem.push(this.tCaracs[i]);
+        }
+      }
+    }
+    
+  }
+
+/*  Initialisation du menu de prix des runes :
+ *    TRIGGER : recherche correcte
+ *    - Prend le tableau de stats en paramètre
+ *    - Initialise le menu déroulant avec les dépendances aux champs de texte
+ *    - Respecte un "template" Dofus-Responsive (ordre prédéfini quelque soit l'item)
+ */
+  initialiserMenuPrix(): void {
+
+  }
+
+/*  Ajoute une ligne de statistique en exo :
+ *    TRIGGER : bouton [Ajouter un Exo]
+ *    - Prend une stat en paramètre
+ *    - Ajoute la ligne de stat au menu de prix des runes
+ *    - Ajoute la ligne de FM correspondante supplémentaire
+ *    - Crée les dépendances supplémentaires nécéssaires
+ */
+  ajouterLigneExo(): void {
+
+  }
+
+/*  Calcule le prix de revient de la FM :
+ *    TRIGGER : tous les champs sont remplis
+ *    - Préviens l'utilisateur que tous les champs ne sont pas remplis sinon
+ *    - Calcule le prix de revient de l'item & l'affiche si toutes les dépendances
+ *      sont bien conçues
+ */
+  calculerPrixTotal(): void {
+
   }
 
 }
+
+/* Plan de jeu :
+      - Définir précisément le schéma front (les *ngFor)
+      - Définir les collections à utiliser
+      - Récupérer le contenu des collections :
+        - Requête REST à l'APi
+        - Récupération & stockage en BDD
+          - MongoDB & Atlas ? (tuto Umtice)
+          - Utilisation de la BDD
+          || Requête API + précise
+      - Variables à créer :
+        - Enum caractéristiques ?
+        - []Ensemble des Items (+stockage)
+        - Nom de l'item
+        - []Caractéristiques de l'item (?)
+        - [nomrune][caractéristique][nombredebut][nombrefin][prixrune]
+        - Exo
+        - PrixTotal
+      - Récupérer le nom de l'item depuis l'input texte
+      - Filtre dans la BDD de l'ensemble des items
+        || Requete API précise et *ngIf(==1 resultat): affichage
+      - Récupération du contenu des zones de textes générées pour le FM
+        - Stockage dans le mégatableau de runes
+      - Récupération du contenu des zones de textes générées pour le prix
+        - Stockage dans le mégatableau de runes
+      - Gestion du bouton d'ajout d'exo (+1 ligne tableau carac item)
+      - Calcul du prix de l'item
+      - Création d'un bouton pour passer au prochain item
+      - FIN?
+*/
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,8 +8,9 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 })
 export class ApiService {
 
-
   REST_API: string = 'https://fr.dofus.dofapi.fr/equipments?filter[where][level]=199';
+  REQUEST_ALL_DOFAPI: string = 'https://fr.dofus.dofapi.fr/equipments';
+  REQUEST_BOTTES_HARRY: string = 'https://fr.dofus.dofapi.fr/equipments?filter[where][name]=Bottes%20Harry'
 
   constructor( private httpClient: HttpClient) { }
 
@@ -18,5 +19,101 @@ export class ApiService {
     return this.httpClient.get(`${this.REST_API}`);
   }
 
-  
+  GetItemsDofapi() {
+    console.log(this.httpClient.get(`${this.REQUEST_ALL_DOFAPI}`));
+    return this.httpClient.get(`${this.REQUEST_ALL_DOFAPI}`);
+  }
+
+  GetBottesHarry(){
+    console.log(this.httpClient.get(`${this.REQUEST_BOTTES_HARRY}`));
+    return this.httpClient.get(`${this.REQUEST_BOTTES_HARRY}`);
+  }
+
+  getStatistiques(){
+    return [
+      new Carac("Vitalité", "Vi", 3),
+      new Carac("Force", "Fo", 3),
+      new Carac("Intelligence", "Ine", 3),
+      new Carac("Chance", "Cha", 3),
+      new Carac("Agilité", "Age", 3),
+      new Carac("Sagesse", "Sa", 3),
+      new Carac("Prospection","Prospe", 2),
+      new Carac("Puissance", "Pui", 3),
+      new Carac("Puissance Pièges", "Pui Pi", 3),
+      new Carac("Résistance Terre", "Ré Terre", 2),
+      new Carac("Résistance Feu", "Ré Feu", 2),
+      new Carac("Résistance Eau", "Ré Eau", 2),
+      new Carac("Résistance Air", "Ré Air", 2),
+      new Carac("Résistance Neutre", "Ré Neutre", 2),
+      new Carac("% Résistance Terre", "Ré per Terre", 1),
+      new Carac("% Résistance Feu", "Ré per Feu", 1),
+      new Carac("% Résistance Eau", "Ré per Eau", 1),
+      new Carac("% Résistance Air", "Ré per Air", 1),
+      new Carac("% Résistance Neutre", "Ré per Neutre", 1),
+      new Carac("Résistance Poussée", "Ré Pou", 2),
+      new Carac("Résistance Critique", "Ré Cri", 2),
+      new Carac("Pods", "Pod", 3),
+      new Carac("Tacle", "Tac", 2),
+      new Carac("Fuite", "Fui", 2),
+      new Carac("Dommages", "Do", 1),
+      new Carac("Dommages Terre", "Do Terre", 2),
+      new Carac("Dommages Feu", "Do Feu", 2),
+      new Carac("Dommages Eau", "Do Eau", 2),
+      new Carac("Dommages Air", "Do Air", 2),
+      new Carac("Dommages Neutre", "Do Neutre", 2),
+      new Carac("Dommages Critique", "Do Cri", 2),
+      new Carac("Dommages Poussée", "Do Pou", 2),
+      new Carac("Dommages Pièges", "Do Pi", 2),
+      new Carac("Dommages Distance", "Do Per Di", 1),
+      new Carac("Dommages Mélée", "Do Per Mé", 1),
+      new Carac("Dommages aux Sorts", "Do Per So", 1),
+      new Carac("% Critique", "Cri", 1),
+      new Carac("Soins", "So", 2),
+      new Carac("Dommages Renvoi", "Do Ren", 1),
+      new Carac("Invocation", "Invo", 1),
+      new Carac("Portée", "PO", 1),
+      new Carac("PA", "Ga PA", 1),
+      new Carac("PM", "Ga PM", 1)
+    ];
+  }
+}
+
+export class Carac{
+  nom: string;
+  runes: Array<Rune>;
+
+  constructor(nom: string, dim: string, n: number){
+    this.nom = nom;
+    this.runes = new Array<Rune>();
+    for (let i = 0; i < n; i++) {
+      this.runes.push( new Rune(dim, i) );      
+    }
+  }
+}
+
+export class Rune{
+  nom: string;
+  nDebut: number;
+  nFin: number;
+  prix: number;
+
+  constructor(nom: string, n: number){
+    switch(n){
+      case 0:  this.nom = nom; break;
+      case 1:  this.nom = "Pa " + nom; break;
+      case 2:  this.nom = "Ra " + nom; break;
+      default: this.nom = "missingN0"; break;
+    }
+    this.nDebut = 0;
+    this.nFin = 0;
+    this.prix = 0;
+  }
+
+  // Méthodes
+  setNombreRuneDeb(n: number): void{ this.nDebut = n; }
+  setNombreRuneFin(n: number): void{ this.nFin = n; }
+  setPrixRune(p: number): void{ this.prix = p; }
+  calculePrix(): number{
+    return( (this.nFin - this.nDebut) * this.prix );
+  }
 }
